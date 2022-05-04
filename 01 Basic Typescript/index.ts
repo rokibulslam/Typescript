e7a872c32b286572199c3bd96e0133cf1c3ab486
rokibulslam <rokibulslam6@gmail.com>
2022-05-04 09:50:43 +0600

    react Typescript

--- 01 Basic Typescript/index.ts
+++ 01 Basic Typescript/index.ts
@@ -28,10 +28,37 @@ add(3, 4);
 add(54, 5);
 
 // Array 
+const arr3:Array<number | string> = [123, 121, 'tamim']
 const arr1: number[] = [123, 154, 134]
 const arr2: string[] = ['asr', "sfdsd",]
 
 // create or modify type: type typename = type structure 
 type myType = number | string;
 
-const id: myType = "23"
+const id: myType = "23";
+
+// Object 
+
+// Create Object Type
+interface IPerson {
+    name: string;
+    age: number;
+    hobby?: string;         // ? for optional
+}
+
+const person: IPerson ={
+    name: "Tamim",
+    age: 22,
+    hobby: "Programming"
+};
+ 
+// Enum 
+
+enum Colors {
+     color1 = '#fff',
+    color2 = "#fffa",
+    color3 = "#fffas"
+ }
+console.log(Colors.color1)
+
+// Generic
